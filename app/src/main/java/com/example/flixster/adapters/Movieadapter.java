60e57024b41d85f55e5bdedc2dc5db7181e206af
Adeel M <callmead@gmail.com>
2020-10-02 20:54:58 -0600

    Final
    Final

--- app/src/main/java/com/example/flixster/adapters/Movieadapter.java
+++ app/src/main/java/com/example/flixster/adapters/Movieadapter.java
@@ -1,4 +1,5 @@
 package com.example.flixster.adapters;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.media.VolumeShaper;
 import android.util.Log;
@@ -7,13 +8,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flixster.DetailActivity;
 import com.example.flixster.R;
 import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
 import java.util.List;
 public class Movieadapter extends RecyclerView.Adapter<Movieadapter.ViewHolder> {
     Context context;
@@ -48,6 +56,7 @@ public class Movieadapter extends RecyclerView.Adapter<Movieadapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -56,6 +65,7 @@ public class Movieadapter extends RecyclerView.Adapter<Movieadapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
         public void bind(Movie movie){
             tvTitle.setText(movie.getTitle());
@@ -70,6 +80,16 @@ public class Movieadapter extends RecyclerView.Adapter<Movieadapter.ViewHolder> 
             }
 
             Glide.with(context).load(imageUrl).into(ivPoster);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context, DetailActivity.class);
+                    // i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
